a9488c1e0d96d7b626c3c10b500a3730f988615b
Alain Patrick Soulyvan EKOGHA MVE NKWELE <121530031+Soulyvan@users.noreply.github.com>
2023-08-19 13:37:18 +0000

    Update generateurCitation.js

--- generateurCitation.js
+++ generateurCitation.js
@@ -20,7 +20,27 @@ let citations = [
     ["Pour gagner votre vie, apprenez à l’école. Pour gagner une fortune, apprenez par vous-même.", "Brian Tracy"],
     ["Les gagnants trouvent des moyens, les perdants des excuses…", "F. D. Roosevelt"],
     ["Vous n’êtes jamais trop vieux pour vous fixer de nouveaux buts, ou rendre vos rêves réalité.", "C.S. Lewis"],
-    ["Un pessimiste voit la difficulté dans chaque opportunité. Un optimiste voit une opportunité dans chaque difficulté.", "Winston Churchill"]
+    ["Un pessimiste voit la difficulté dans chaque opportunité. Un optimiste voit une opportunité dans chaque difficulté.", "Winston Churchill"],
+    ["L'échec est le fondement de la réussite.", "Lao Tseu"],
+    ["La meilleure façon de prédire l'avenir est de le créer.", "Peter Drucker"],
+    ["Le courage n'est pas l'absence de peur, mais la capacité de la vaincre.", "Nelson Mandela"],
+    ["La vie devient ce que l'on décide d'en faire.", "Eleanor Roosevelt"],
+    ["Le seul moyen de faire du bon travail, c'est d'aimer ce que vous faites.", "Steve Jobs"],
+    ["Soyez vous-même, tous les autres sont déjà pris.", "Oscar Wilde"],
+    ["Le plus grand secret du bonheur, c'est d'être bien avec soi-même.", "Bernard Fontenelle"],
+    ["L'éducation est l'arme la plus puissante pour changer le monde.", "Nelson Mandela"],
+    ["Le bonheur est la seule chose qui se double si on le partage.", "Albert Schweitzer"],
+    ["Le succès, c'est de passer d'échec en échec sans perdre son enthousiasme.", "Winston Churchill"],
+    ["Le talent, c'est d'avoir envie.", "Jacques Brel"],
+    ["La vraie générosité envers l'avenir consiste à tout donner au présent.", "Albert Camus"],
+    ["On ne voit bien qu'avec le cœur. L'essentiel est invisible pour les yeux.", "Antoine de Saint-Exupéry"],
+    ["Rien n'est impossible, le mot lui-même dit 'je suis possible' !", "Audrey Hepburn"],
+    ["La persévérance n'est pas une longue course, mais plusieurs petites courses dans la même direction.", "Walter Elliot"],
+    ["La folie, c'est de refaire la même chose et d'attendre des résultats différents.", "Albert Einstein"],
+    ["La plus grande gloire n'est pas de ne jamais tomber, mais de se relever à chaque chute.", "Confucius"],
+    ["L'imagination est plus importante que la connaissance.", "Albert Einstein"],
+    ["Si vous voulez connaître un homme, observez comment il traite ses inférieurs, pas ses égaux.", "J.K. Rowling"],
+    ["La seule façon de faire du bon travail est d'aimer ce que vous faites.", "Steve Jobs"]
 ];
 
 let citation = document.getElementById('citation');
